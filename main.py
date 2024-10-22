9bec754615f3111fdfc94c37dff77c03263fa0b8
Sammy <bavesh2006kumar@gmail.com>
2024-10-22 22:12:32 +0000

    final

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
-# pip install pywebio requests beautifulsoup4
 import requests
-from bs4 import BeautifulSoup  # Use for HTML parsing
-from pywebio.input import *
 from pywebio.output import *
-from pywebio.session import *
 
-def get_fun_fact(_):
-    clear()  # To clear the screen
-    put_html('<p><h1>Fun Fact</h1></p>')
+def get_random_joke(_):
+    clear()  # Clear the screen each time
+    put_html('<p><h1><img src="https://i.pinimg.com/564x/8e/12/57/8e12576ae30e944a0fde1639c2cc42e9.jpg" width="27%">Random Joke</h1></p>')
 
     url = "https://official-joke-api.appspot.com/random_joke"
-
-    # Fetching HTML data from the URL
     response = requests.get(url)
 
-    # Use BeautifulSoup to extract the fun fact from HTML
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # Check if the request was successful
+    if response.status_code == 200:
+        joke_data = response.json()  # Parse JSON response
+        setup = joke_data['setup']   # Get the setup of the joke
+        punchline = joke_data['punchline']  # Get the punchline of the joke
 
-    # Example: find a specific fact from a specific tag or class
-    # You need to inspect the website to find the right element containing the fact
-    fun_fact = soup.get_text()  # For demonstration, extracting the first <li> fact
+        # Display the joke in a formatted way
+        joke = f"{setup}\n... {punchline}"
+        style(put_text(joke), 'color:green; font-size: 20px')
+    else:
+        put_text("Failed to fetch a joke. Try again.")
 
-    # Display the fun fact
-    style(put_text(fun_fact), 'color:blue; font-size: 30px')
-
-    # Add the button again to fetch a new fact
-    put_buttons(
-        [dict(label='Click me', value='outline-success', color='outline-success')],
-        onclick=get_fun_fact
-    )
+    put_buttons([dict(label='Get another joke', value='joke')], onclick=get_random_joke)
 
 if __name__ == '__main__':
-    # Display the header
-    put_html(
-        '<p align="left">'
-        '<h2><img src="https://i.pinimg.com/564x/8e/12/57/8e12576ae30e944a0fde1639c2cc42e9.jpg" width="27%"> Fun Fact Generator </h2>'
-        '</p>'
-    )
-
-    # Add the button to start fetching fun facts
-    put_buttons(
-        [dict(label='Click me', value='outline-success', color='outline-success')],
-        onclick=get_fun_fact
-    )
-
-    # Hold the session to keep it running
-    hold()
+    put_html('<p><h1><img src="https://i.pinimg.com/564x/8e/12/57/8e12576ae30e944a0fde1639c2cc42e9.jpg" width="27%">Random Joke</h1></p>')
+    put_buttons([dict(label='Get a random joke', value='joke')], onclick=get_random_joke)
